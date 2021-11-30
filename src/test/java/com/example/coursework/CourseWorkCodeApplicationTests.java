1cefe96c6eb7fca148c7bf2d6db486fd734d1921
Alexander Kanenkov <al.kanenckoff@yandex.ru>
2021-11-30 05:00:28 +0300

    add tests

--- src/test/java/com/example/coursework/CourseWorkCodeApplicationTests.java
+++ src/test/java/com/example/coursework/CourseWorkCodeApplicationTests.java
@@ -1,13 +1,48 @@
 package com.example.coursework;
 
+import com.example.coursework.Data.Repositories.UserRepository;
+import com.example.coursework.Services.AuthorizationService;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 class CourseWorkCodeApplicationTests {
 
+    @Autowired
+    private MockMvc mockMvc;
+
     @Test
-    void contextLoads() {
+    public void testGettingHomePage() throws Exception {
+        this.mockMvc.perform(get("/"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testRedirectionToLoginWithoutAuthorization() throws Exception {
+        this.mockMvc.perform(get("/main"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("http://localhost/login"));
+    }
+
+    @Test
+    @Sql(value = "/insert_user_before_test.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    public void testAuthorization() throws Exception {
+        this.mockMvc.perform(formLogin().user("test_user").password("qwerty"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/main"));
     }
 
 }
---
+++ src/test/resources/insert_user_before_test.sql
@@ -0,0 +1,4 @@
+delete from users where username='test_user';
+
+insert into users(username, email, password, phone_number)
+values ('test_user', 'test@gmail.com', '$2a$12$qN5aWhIJp5UsH28E8REi1eLVDibpSPFVnJWyzDToQp/UDTu0IT7ha', '+79327564196');
